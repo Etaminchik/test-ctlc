cca0e45651b72c91bacbacc8557d4627be18e197
Etaminchik <43892972+Etaminchik@users.noreply.github.com>
2023-12-22 14:19:55 +0300

    Update bundles_aaa.py
    date_h = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d 00:00:00")

--- lib/scenariuos/oimc_broadband/bundles_aaa.py
+++ lib/scenariuos/oimc_broadband/bundles_aaa.py
@@ -83,7 +83,8 @@ def run(cur_,telco_codes_,native_partitions_,range_,exclude_client_address_,thre
         date_h = (datetime.today()).strftime("%Y-%m-%d 00:00:00")
     else:
         date_l = (datetime.today() - timedelta(hours=range_)).strftime("%Y-%m-%d %H:%M:%S")
-        date_h = (datetime.today()).strftime("%Y-%m-%d %H:%M:%S")
+        date_h = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d 00:00:00")
+
 
     if native_partitions_:template_partitions = template_partitions_new
     else:template_partitions = template_partitions_old
