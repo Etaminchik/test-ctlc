66df4cbd0755c87da54a2ecb501b9c5e3dabdde9
Etaminchik <43892972+Etaminchik@users.noreply.github.com>
2024-07-24 11:42:55 +0300

    Update bundles_aaa.py

--- lib/scenariuos/oimc_broadband/bundles_aaa.py
+++ lib/scenariuos/oimc_broadband/bundles_aaa.py
@@ -72,8 +72,8 @@ def optional(type_,exclude_,telco_codes_):
     result = ""
     if exclude_[0] != '[]' or exclude_[0] != '':result += f""" and not {type_}_client_address <<= any (array{exclude_[0]}::inet[])"""
     if exclude_[1] != '[]' or exclude_[1] != '':result += f""" and not {type_}_server_address <<= any (array{exclude_[1]}::inet[])"""
-    if exclude_[2] == 'True':result+= f""" and not ${type_}_client_address <<= any(select oinp_subnet from oims.oper_ip_numbering_plan_history where oinp_oper_id in (${telco_codes_}) and oinp_description ilike '%Служебные%')"""
-    if exclude_[2] == 'True':result+= f""" and not ${type_}_server_address <<= any(select oinp_subnet from oims.oper_ip_numbering_plan_history where oinp_oper_id in (${telco_codes_}) and oinp_description ilike '%Служебные%')"""
+    if exclude_[2] == 'True':result+= f""" and not ${type_}_client_address <<= any(select oinp_subnet from oims.oper_ip_numbering_plan_history where oinp_oper_id in (${telco_codes_}) and oinp_description ilike '%Служебн%')"""
+    if exclude_[2] == 'True':result+= f""" and not ${type_}_server_address <<= any(select oinp_subnet from oims.oper_ip_numbering_plan_history where oinp_oper_id in (${telco_codes_}) and oinp_description ilike '%Служебн%')"""
     return result
 
 def info():
@@ -106,7 +106,7 @@ def run(cur_,telco_codes_,native_partitions_,range_,exclude_client_address_,excl
                                            telco_codes=np.array2string(telco_codes_[:,1]).replace('[','').replace(']','').replace(' ',','),
                                            optional=optional(type_part[part[1]],
                                                              [exclude_client_address_,exclude_server_address_,aaa_exclude_services_subnets_from_ip_numbering_],
-                                                             telco_codes=np.array2string(telco_codes_[:,1]).replace('[','').replace(']','').replace(' ',',')))
+                                                             np.array2string(telco_codes_[:,1]).replace('[','').replace(']','').replace(' ',',')))
         
         logging.debug(f"""SELECT: {__select}""")
         cur_.execute(__select)
